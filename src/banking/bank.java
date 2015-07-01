a90087f348ab644229b26714c07eb57828a9f29a
vikramaditya09 <vikramaditya.tanwar@magicsw.com>
2015-07-01 14:47:13 +0530

    accounts,customer,transaction

---
+++ src/banking/bank.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banking;
+import java.util.*;
+
+/**
+ *
+ * @author vikramaditya.tanwar
+ */
+public class bank {
+    void list(){
+  List<Customer> a=new LinkedList<>();
+  a.add(new Customer("vikram","janakpuri",1001,02));
+}
+}
